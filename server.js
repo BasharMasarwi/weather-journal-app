5ed0cb5f379f803f97811706f494bf18a6973ca3
BasharMasarwi <bbbb14bashar@gmail.com>
2024-07-21 04:44:09 +0300

    first

---
+++ server.js
@@ -0,0 +1,61 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+ const express = require('express');
+ const app = express();
+ const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`server running`);
+    console.log(`running on localhost: ${port}`);
+}
+
+app.get('/all', function (req, res) {
+res.send(projectData);
+projectData = [];
+});
+
+app.post('/add', addData);
+
+function addData(req, res) {
+   console.log(req.body);
+   newData ={
+    data : req.body.data,
+    temp : req.body.temp,
+    content : req.body.content
+   }
+   projectData.push(newData);   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ website/app.js
@@ -0,0 +1,71 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=1b5fe91b6fec40c9d2b88539872d1846';
+
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+
+function performAction(e){
+    const zip = document.getElementById('zip').value;
+    const fellings = document.getElementById('feelings').value;
+
+    getWeatherData(baseURL, zip, apiKey)
+    .then(function(data){
+        console.log(data);
+        postData('/add', {date: newDate, temp:data.list[0].main.temp, content: fellings});
+        updateUI();
+    })
+}
+
+// Function to GET Web API Data
+const getWeatherData = async (baseURL, zip, apiKey)=>{
+    const res = await fetch(baseURL+zip+apiKey);
+    try{
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }catch(error){
+        console.log("error", error);
+    }   
+}
+
+// Function to POST data
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    try{
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error){
+        console.log("error", error);
+    }
+}   
+
+// Function to GET Project Data
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        console.log(allData);
+        document.getElementById('date').innerHTML = 'Date: '+ allData[0].date;
+        document.getElementById('temp').innerHTML = 'Temperature: '+ allData[0].temp;
+        document.getElementById('content').innerHTML = 'Content: '+ allData[0].content;
+    }catch(error){
+        console.log("error", error);
+    }
+}
